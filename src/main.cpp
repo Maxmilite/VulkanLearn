f5a5f49b92ff2db088b5b96561ed47c9fc109392
Maxmilite <yufe008@live.com>
2024-02-11 21:11:33 +0800

    stash

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,10 @@ int main(int argc, char* argv[]) {
     commandPoolGraphics.allocateBuffers(commandBufferGraphics);
     commandPoolPresentation.allocateBuffers(commandBufferPresentation);
 
+    // CommandBuffer commandBuffer;
+    // CommandPool commandPool(GraphicsBase::getBase().getQueueFamilyIndexGraphics(), VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT);
+    // commandPool.allocateBuffers(commandBuffer);
+
     VkClearValue clearColor = { .color = { 1.f, 0.f, 0.f, 1.f } };
 
     while (!GLFW::shouldClose()) {
@@ -31,10 +35,16 @@ int main(int argc, char* argv[]) {
         GLFW::fps();
 
         GraphicsBase::getBase().swapImage(semaphoreImageIsAvailable);
+        
+        auto i = GraphicsBase::getBase().getCurrentImageIndex();
+        
         fence.waitAndReset();
 
         commandBufferGraphics.begin(VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT);
         GraphicsBase::getBase().cmdTransferImageOwnership(commandBufferGraphics);
+        renderPass.cmdBegin(commandBufferGraphics, framebuffers[i], { {}, windowSize }, clearColor);
+        renderPass.cmdEnd(commandBufferGraphics);
+
         commandBufferGraphics.end();
         GraphicsBase::getBase().submitCommandBufferGraphics(commandBufferGraphics, semaphoreImageIsAvailable);
 
